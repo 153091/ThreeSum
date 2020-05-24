882092691fd9feb7495dfa47cd1d6e7179daab4c
153091 <xpoint06@gmail.com>
2020-05-24 11:02:05 +0300

    пробная версия

--- src/ThreeSum.java
+++ src/ThreeSum.java
@@ -1,7 +1,26 @@
+package src;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
 public class ThreeSum{
 
-    public static int count(int a){
+
+    public static int count(int[] a){
         int n = a.length;
+        int count = 0;
+        for (int i = 0; i<n; i++)
+            for (int j = i + 1; j<n; j++)
+                for (int k = j +1; k<n; k++)
+                    if(a[i] + a[j] + a[k] == 0)
+                        count++;
+                    return count;
+
+    }
+
+    public static void main(String[] args) {
+        int[] a = In.readInts(args[0]);
+        StdOut.println(count(a));
 
     }
 }
